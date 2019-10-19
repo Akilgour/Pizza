4d5faa570ba073cb1214817f099847bcf3d6c88c
Ashley Kilgour <kilgourkilgour@hotmail.com>
2019-10-19 22:10:05 +0100

    Added a admin project

---
+++ Pizza.Admin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pizza.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
